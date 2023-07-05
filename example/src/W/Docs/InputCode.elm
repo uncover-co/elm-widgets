43760a38730d9d0ec516db095b9f843e3468e681
Georges Boris <georgesboris@gmail.com>
2023-07-06 00:51:21 +0200

    ongoing

---
+++ example/src/W/Docs/InputCode.elm
@@ -0,0 +1,33 @@
+module W.Docs.InputCode exposing (Model, init, docs)
+
+import ElmBook.Chapter exposing (Chapter, chapter, renderStatefulComponentList)
+import ElmBook.Actions exposing (updateState)
+import Html as H
+import Html.Attributes as HA
+import W.InputCode
+
+
+type alias Model
+    = String
+
+
+init : Model
+init =
+    ""
+
+
+docs : Chapter { x | inputCode : Model }
+docs =
+    chapter "W.InputCode"
+        |> renderStatefulComponentList
+            [ ( "Default"
+              , \{ inputCode } ->
+                    W.InputCode.view []
+                        { length = 6
+                        , value = inputCode
+                        , onInput =
+                            \input ->
+                                updateState (\state -> { state | inputCode = input })
+                        }
+              )
+            ]
--- src/W/InputCode.elm
+++ src/W/InputCode.elm
@@ -1,33 +1,133 @@
 module W.InputCode exposing (view)
 
-import Array
+import Array exposing (Array)
 import Html as H
 import Html.Attributes as HA
+import Html.Events as HE
 
 
+-- Attributes
+
+
+{-| -}
+type Attribute msg
+    = Attribute (Attributes msg -> Attributes msg)
+
+
+type alias Attributes msg =
+    { uppercase : Bool
+    , hiddenCharacters : Bool
+    , onDone : Maybe msg
+    }
+
+
+defaultAttrs : Attributes msg
+defaultAttrs =
+    { uppercase = False
+    , hiddenCharacters = False
+    , onDone = Nothing
+    }
+
+
+applyAttrs : List (Attribute msg) -> Attributes msg
+applyAttrs attrs =
+    List.foldl (\(Attribute fn) a -> fn a) defaultAttrs attrs
+
+
+
+-- Attributes : Setters
+
+
+{-| -}
+uppercase : Attribute msg
+uppercase =
+    Attribute (\attrs -> { attrs | uppercase = True })
+
+
+{-| -}
+hiddenCharacters : Attribute msg
+hiddenCharacters =
+    Attribute (\attrs -> { attrs | hiddenCharacters = True })
+
+
+{-| -}
+onDone : msg -> Attribute msg
+onDone v =
+    Attribute (\attrs -> { attrs | onDone = Just v })
+
+
+{-| -}
+noAttr : Attribute msg
+noAttr =
+    Attribute identity
+
+
+
+-- Main
+
+{-| -}
 view :
-    { length : Int
+    List (Attribute msg)
+    -> { length : Int
     , value : String
+    , onInput : String -> msg
     }
     -> H.Html msg
-view props =
+view attrs_ props =
     if props.length <= 0 then
         H.div [] []
 
     else
         let
+            attrs : Attributes msg
+            attrs =
+                applyAttrs attrs_
+
             valueLetters : Array String
             valueLetters =
                 props.value
                     |> String.split ""
+                    |> List.take props.length
                     |> Array.fromList
+
+            activeIndex : Int
+            activeIndex =
+                Array.length valueLetters
         in
         H.label
-            []
-            [ H.input [] []
-            , H.div
+            [ HA.class "ew-group/ew" ]
+            [ H.input
+                [ HA.class "ew-h-0 ew-p-0 ew-m-0 ew-border-0"
+                , HA.value props.value
+                , HE.onFocus (props.onInput "")
+                , HE.onInput
+                    (\value ->
+                        value
+                        |> String.left props.length
+                        |> props.onInput
+                    )
+                ]
                 []
-                (props.length
+            , H.div
+                [ HA.class "ew-flex ew-gap-2"]
+                ((props.length - 1)
                     |> List.range 0
+                    |> List.map (\index ->
+                        H.div
+                            [ HA.class "ew-border-solid ew-border-base-aux/30 ew-border-2"
+                            , HA.class "ew-w-10 ew-h-14 ew-rounded-md"
+                            , HA.class "ew-flex ew-items-center ew-justify-center"
+                            , HA.class "ew-font-heading ew-text-3xl ew-text-base-fg"
+                            , HA.classList
+                                [ ( "group-focus-within/ew:ew-border-base-fg"
+                                  , index == activeIndex
+                                  )
+                                ]
+                            ]
+                            [ Array.get index valueLetters
+                                |> Maybe.withDefault ""
+                                |> H.text
+                            ]
+                    )
                 )
             ]
